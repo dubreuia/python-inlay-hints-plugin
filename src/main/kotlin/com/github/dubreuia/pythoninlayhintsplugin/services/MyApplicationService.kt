0f65cda7a032507a54ea17c2f64a8e6f8228fff2
GitHub Action <action@github.com>
2021-12-05 16:47:33 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/dubreuia/pythoninlayhintsplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.dubreuia.pythoninlayhintsplugin.services
+
+import com.github.dubreuia.pythoninlayhintsplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
